7fc80e4b7777c7091d15ab23f3e5738576e4bff5
javierpb275 <javierpb275@gmail.com>
2022-03-26 14:55:49 +0100

    the TEXT Macro

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,14 +5,15 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     //Showing messages when start playing on the sign of the game
-    PrintLine("Welcome to Bull Cows!");
-    PrintLine("Press enter to continue...");
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     //Removes messages and clears the sign of the game
     ClearScreen();
-    FString HiddenWord = 'cake';
+    //TEXT() macro: so that unreal can encode it correctly
+    FString HiddenWord = TEXT("cake");
     
 }
