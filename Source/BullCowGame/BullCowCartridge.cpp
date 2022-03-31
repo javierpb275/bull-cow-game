96162c35cd76667a20c17b9145a563405cf3da6c
javierpb275 <javierpb275@gmail.com>
2022-03-31 18:41:08 +0200

    created initgame function

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,46 +5,50 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    //Showing messages when start playing on the sign of the game
+    // Showing messages when start playing on the sign of the game
     PrintLine(TEXT("Welcome to Bull Cows!"));
-    PrintLine(TEXT("Guess the 4 letter word!"));//Magic Number Remove
+    PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number Remove
     PrintLine(TEXT("Press enter to continue..."));
 
-    //Setting up game
-    HiddenWord = TEXT("cake");// Set the HiddemWord
-    //Set Lives
+    InitGame(); // Setting up game
 
-    //Prompt player for guess
+    // Prompt player for guess
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
-    //Removes messages and clears the sign of the game
+    // Removes messages and clears the sign of the game
     ClearScreen();
 
-    //checking player guess
+    // checking player guess
 
-    //check if it is the correct hidden word
-    if (Input == HiddenWord) 
+    // check if it is the correct hidden word
+    if (Input == HiddenWord)
     {
         PrintLine(TEXT("You have won!"));
     }
-    else 
+    else
     {
         PrintLine(TEXT("You have lost!"));
     };
-    //Check if Isogram
-    //prompt to guess again
-    //Check right number of characters
-    //prompt to guess again
+    // Check if Isogram
+    // prompt to guess again
+    // Check right number of characters
+    // prompt to guess again
 
-    //Remove Life
-    
-    //Check if lives > 0
-    //if yes guess again
-    //show lives left
-    //if no show game over and hiddenwod?
-    //prompt to play again, press enter to play again?
-    //check user input
-    //playgain or guit
+    // Remove Life
+
+    // Check if lives > 0
+    // if yes guess again
+    // show lives left
+    // if no show game over and hiddenwod?
+    // prompt to play again, press enter to play again?
+    // check user input
+    // playgain or guit
+}
+
+void UBullCowCartridge::InitGame()
+{
+    HiddenWord = TEXT("cake");
+    Lives = 4;
 }
